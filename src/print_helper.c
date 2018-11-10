60fa19d168b449f8f594382b0c4fc9aa2f476b01
Allar Vendla <allarvendla@gmail.com>
2018-11-10 21:35:57 +0200

    Add print helper for printing banner from program memory.

---
+++ src/print_helper.c
@@ -0,0 +1,11 @@
+#include <avr/pgmspace.h>
+#include <stdio.h>
+
+void print_banner_P(FILE *stream, const char * const * banner, const uint8_t rows)
+{
+    for (uint8_t row = 0; row < rows; row++) {
+        fprintf_P(stream, (PGM_P) pgm_read_word(&(banner[row])));
+        fprintf(stream, "\n");
+    }
+    fprintf(stream, "\n");
+}
---
+++ src/print_helper.h
@@ -0,0 +1,7 @@
+#ifndef PRINT_HELPER_H
+#define PRINT_HELPER_H
+#include <stdio.h>
+
+void print_banner_P(FILE *stream, const char * const * banner, const uint8_t rows);
+
+#endif /* PRINT_HELPER_H */
