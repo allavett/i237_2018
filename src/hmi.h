970b602799660b316604ed8e2a8b6571ae82ae37
Allar Vendla <allarvendla@gmail.com>
2018-11-10 13:01:27 +0200

    Add used text to program memory.

---
+++ src/hmi.h
@@ -0,0 +1,9 @@
+#ifndef HMI_H
+#define HMI_H
+
+#define STUDENT_NAME "Allar Vendla\n"
+#define VER_FV "Version: " FW_VERSION " built on: " __DATE__ " " __TIME__ "\n"
+#define VER_LIBC "avr-libc version: " __AVR_LIBC_VERSION_STRING__ " avr-gcc version: " __VERSION__ "\n"
+#define INPUT_ASK_LETTER "Enter Month name first letter >"
+
+#endif /* HMI_H */
